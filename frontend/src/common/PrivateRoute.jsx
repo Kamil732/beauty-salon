51034fc49e477bb17201fc0e50c0b8d032c9c479
Kamil Głuchowski <kamilkamil5542@wp.pl>
2021-03-28 20:46:50 +0200

    added: common files

---
+++ frontend/src/common/PrivateRoute.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { Redirect, Route } from 'react-router-dom'
+
+class PrivateRoute extends Component {
+    static propTypes = {
+        isAuthenticated: PropTypes.bool,
+        userLoading: PropTypes.bool,
+    }
+
+    render() {
+        if (this.props.userLoading !== false)
+            return <></>
+        else if (this.props.userLoading === false && this.props.isAuthenticated === false)
+            return <Redirect to="/login" />
+
+        return <Route {...this.props}>{this.props.children}</Route>
+    }
+}
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated,
+    userLoading: state.auth.loading,
+})
+
+
+export default connect(mapStateToProps, null)(PrivateRoute)
