43103878a57078c1f78d565411376922290fbd6c
Kamil Głuchowski <kamilkamil5542@wp.pl>
2021-03-30 12:40:08 +0200

    styled: buttons

--- frontend/src/containers/layout/buttons/Button.jsx
+++ frontend/src/containers/layout/buttons/Button.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Button(props) {
-	return (
-		<button
-			className={`btn${props.primary ? ' btn__primary' : ''} ${
-				props.secondary ? ' btn__secondary' : ''
-			}`}
-		>
-			{props.children}
-		</button>
-	)
+	let classNames = 'btn'
+
+	if (props.primary) classNames += ' btn__primary'
+	if (props.secondary) classNames += ' btn__secondary'
+	if (props.to) {
+		classNames += ' btn__link'
+
+		return (
+			<Link to={props.to} className={classNames}>
+				{props.children}
+			</Link>
+		)
+	}
+
+	return <button className={classNames}>{props.children}</button>
 }
 
 export default Button
