f0853198e5b680b5efbd17da9d1f96429ff7972f
Kamil Głuchowski <kamilkamil5542@wp.pl>
2021-04-26 22:19:24 +0200

    added: populate_meetings helper command

---
+++ backend/server/meetings/management/commands/populate_meetings.py
@@ -0,0 +1,100 @@
+import datetime
+import random
+from faker import Faker
+
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+from server.utilities import get_working_hours
+from accounts.models import Account
+from meetings.models import Meeting
+
+
+class Command(BaseCommand):
+    help = 'Populate meetings'
+
+    def add_arguments(self, parser):
+        parser.add_argument('start', nargs='+', type=str)
+        parser.add_argument('end', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        faker = Faker('pl_PL')
+        self.stdout.write(self.style.WARNING('\n\nPopulating meetings DATABASE\n'))
+
+        current_date = datetime.datetime.strptime(options['start'][0], '%Y-%m-%d')
+
+        end_date = datetime.datetime.strptime(options['end'][0], '%Y-%m-%d')
+
+        barber1 = Account.objects.get(is_admin=True, email='kamilkamil554@wp.pl')
+        barber2 = Account.objects.get(is_admin=True, email='fryzjer@fryzjer.pl')
+        barber3 = Account.objects.get(is_admin=True, email='fryzjer2@fryzjer2.pl')
+
+        while current_date <= end_date:
+            work_hours = get_working_hours(current_date.weekday(), False)
+
+            if not(work_hours['is_non_working_hour']):
+                current_time = current_date.replace(
+                    hour=int(work_hours['start'].split(':')[0]), minute=int(work_hours['start'].split(':')[1]))
+                end_time = current_date.replace(hour=int(work_hours['end'].split(
+                    ':')[0]), minute=int(work_hours['end'].split(':')[1]))
+
+                while current_time < end_time:
+                    customer1, _ = Account.objects.get_or_create(
+                        email=faker.unique.email(),
+                        first_name=faker.first_name(),
+                        last_name=faker.last_name(),
+                        phone_number='+48500484315',
+                        trusted_customer=faker.boolean()
+                    )
+
+                    customer2, _ = Account.objects.get_or_create(
+                        email=faker.unique.email(),
+                        first_name=faker.first_name(),
+                        last_name=faker.last_name(),
+                        phone_number='+48500484315',
+                        trusted_customer=faker.boolean()
+                    )
+
+                    customer3, _ = Account.objects.get_or_create(
+                        email=faker.unique.email(),
+                        first_name=faker.first_name(),
+                        last_name=faker.last_name(),
+                        phone_number='+48500484315',
+                        trusted_customer=faker.boolean()
+                    )
+
+                    Meeting.objects.create(
+                        barber=barber1,
+                        customer=customer1,
+                        customer_first_name=customer1.first_name,
+                        customer_last_name=customer1.last_name,
+                        customer_phone_number=customer1.phone_number,
+                        type=random.choice(['hair', 'beard']),
+                        start=current_time
+                    )
+
+                    Meeting.objects.create(
+                        barber=barber2,
+                        customer=customer2,
+                        customer_first_name=customer2.first_name,
+                        customer_last_name=customer2.last_name,
+                        customer_phone_number=customer2.phone_number,
+                        type=random.choice(['hair', 'beard']),
+                        start=current_time
+                    )
+
+                    Meeting.objects.create(
+                        barber=barber3,
+                        customer=customer3,
+                        customer_first_name=customer3.first_name,
+                        customer_last_name=customer3.last_name,
+                        customer_phone_number=customer3.phone_number,
+                        type=random.choice(['hair', 'beard']),
+                        start=current_time
+                    )
+
+                    current_time += datetime.timedelta(minutes=30)
+
+            current_date += datetime.timedelta(days=1)
+
+        self.stdout.write(self.style.SUCCESS('\nMeetings DATABASE was populated\n\n'))
--- backend/server/server/utilities.py
+++ backend/server/server/utilities.py
@@ -4,7 +4,7 @@ from django.db.models import Q
 from data.models import Data
 
 
-def get_working_hours(week_day):
+def get_working_hours(week_day, translated_time=True):
     is_non_working_hour = False
     start = end = 0
 
@@ -16,51 +16,54 @@ def get_working_hours(week_day):
         if not(days['start_work_monday']):
             is_non_working_hour = True
         else:
-            start = int(days['start_work_monday'].split(':')[0]) * 60 + int(days['start_work_monday'].split(':')[1])
-            end = int(days['end_work_monday'].split(':')[0]) * 60 + int(days['end_work_monday'].split(':')[1])
+            start = days['start_work_monday']
+            end = days['end_work_monday']
 
     elif (week_day == 1):
         if not(days['start_work_tuesday']):
             is_non_working_hour = True
         else:
-            start = int(days['start_work_tuesday'].split(':')[0]) * 60 + int(days['start_work_tuesday'].split(':')[1])
-            end = int(days['end_work_tuesday'].split(':')[0]) * 60 + int(days['end_work_tuesday'].split(':')[1])
+            start = days['start_work_tuesday']
+            end = days['end_work_tuesday']
 
     elif (week_day == 2):
         if not(days['start_work_wednesday']):
             is_non_working_hour = True
         else:
-            start = int(days['start_work_wednesday'].split(':')[0]) * 60 + \
-                int(days['start_work_wednesday'].split(':')[1])
-            end = int(days['end_work_wednesday'].split(':')[0]) * 60 + int(days['end_work_wednesday'].split(':')[1])
+            start = days['start_work_wednesday']
+            end = days['end_work_wednesday']
 
     elif (week_day == 3):
         if not(days['start_work_thursday']):
             is_non_working_hour = True
         else:
-            start = int(days['start_work_thursday'].split(':')[0]) * 60 + int(days['start_work_thursday'].split(':')[1])
-            end = int(days['end_work_thursday'].split(':')[0]) * 60 + int(days['end_work_thursday'].split(':')[1])
+            start = days['start_work_thursday']
+            end = days['end_work_thursday']
 
     elif (week_day == 4):
         if not(days['start_work_friday']):
             is_non_working_hour = True
         else:
-            start = int(days['start_work_friday'].split(':')[0]) * 60 + int(days['start_work_friday'].split(':')[1])
-            end = int(days['end_work_friday'].split(':')[0]) * 60 + int(days['end_work_friday'].split(':')[1])
+            start = days['start_work_friday']
+            end = days['end_work_friday']
 
     elif (week_day == 5):
         if not(days['start_work_saturday']):
             is_non_working_hour = True
         else:
-            start = int(days['start_work_saturday'].split(':')[0]) * 60 + int(days['start_work_saturday'].split(':')[1])
-            end = int(days['end_work_saturday'].split(':')[0]) * 60 + int(days['end_work_saturday'].split(':')[1])
+            start = days['start_work_saturday']
+            end = days['end_work_saturday']
 
     elif (week_day == 6):
         if not(days['start_work_sunday']):
             is_non_working_hour = True
         else:
-            start = int(days['start_work_sunday'].split(':')[0]) * 60 + int(days['start_work_sunday'].split(':')[1])
-            end = int(days['end_work_sunday'].split(':')[0]) * 60 + int(days['end_work_sunday'].split(':')[1])
+            start = days['start_work_sunday']
+            end = days['end_work_sunday']
+
+    if translated_time:
+        start = int(start.split(':')[0]) * 60 + int(start.split(':')[1])
+        end = int(end.split(':')[0]) * 60 + int(end.split(':')[1])
 
     return {
         'start': start,
