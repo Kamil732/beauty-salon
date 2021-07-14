bb5677a6f58249349d58259c1d013a3f7515e9a3
Kamil Głuchowski <kamilkamil5542@wp.pl>
2021-07-15 00:14:47 +0200

    add backend virtual env & gitignore

--- backend/server/meetings/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-from . import views
-
-
-urlpatterns = [
-    path('', views.MeetingListAPIView.as_view(), name='meeting-list'),
-    path('<int:meeting_id>/', views.MeetingDetailAPIView.as_view(), name='meeting-detail'),
-]
